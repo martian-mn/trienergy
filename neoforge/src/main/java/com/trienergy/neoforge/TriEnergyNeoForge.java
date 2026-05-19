d7eace08439f1b4a7a87f65a525b113a9f341dd4
puckstop <ashbu.rns75@aliasvault.net>
2026-05-19 13:54:53 -0400

    feat(neoforge): rename entrypoint to TriEnergyNeoForge, wire to TriEnergy.init()
    Drops the generator's ExampleModNeoForge placeholder in favour of
    TriEnergyNeoForge calling TriEnergy.init() directly. Updates
    neoforge.mods.toml to use the MIT license, puckstop as author,
    and a real description.

---
+++ neoforge/src/main/java/com/trienergy/neoforge/TriEnergyNeoForge.java
@@ -0,0 +1,11 @@
+package com.trienergy.neoforge;
+
+import com.trienergy.TriEnergy;
+import net.neoforged.fml.common.Mod;
+
+@Mod(TriEnergy.MOD_ID)
+public final class TriEnergyNeoForge {
+    public TriEnergyNeoForge() {
+        TriEnergy.init();
+    }
+}
